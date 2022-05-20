65b3940d21baef6ec43d625c34aabf9bab3bd505
Joshua-Ronald Cruz <h702785810@adams019a-mac09.hofstra.edu>
2022-05-19 20:33:24 -0400

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Joshua-Ronald Cruz on 5/19/22.
+//  Copyright © 2022 Joshua-Ronald Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
